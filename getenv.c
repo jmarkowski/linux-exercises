4be8db41774cbe0d441b057baaadb04413d7981e
Jan Markowski <jan@markowski.ca>
2017-04-24 21:47:31 -0400

    Reentrant implementation of getenv

--- getenv.c
+++ getenv.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <pthread.h>
 
 extern char **environ;
 
@@ -27,6 +29,59 @@ char * getenv(const char *name)
     return NULL;
 }
 
+/*******************************************************************************
+ * Reentrant implementation of getenv
+ ******************************************************************************/
+pthread_mutex_t envMtx;
+
+static pthread_once_t initDone = PTHREAD_ONCE_INIT;
+
+static void threadInit(void)
+{
+    pthread_mutexattr_t attr;
+
+    /* Configure the mutex attributes */
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+
+    /* Initialize the environment mutex with given attributes */
+    pthread_mutex_init(&envMtx, &attr);
+
+    pthread_mutexattr_destroy(&attr);
+}
+
+int getenv_r(const char *name, char *buf, int buflen)
+{
+    int k, len, envValLen;
+
+    /* The following is called only once, ever */
+    pthread_once(&initDone, threadInit);
+
+    len = strlen(name);
+
+    pthread_mutex_lock(&envMtx);
+
+    for (k = 0; environ[k] != NULL; k++) {
+        /* return the value after the '=' */
+        if ((strncmp(name, environ[k], len) == 0) && (environ[k][len] == '=')) {
+            envValLen = strlen(&environ[k][len+1]);
+
+            if (envValLen >= buflen) {
+                pthread_mutex_unlock(&envMtx);
+                return ENOSPC;  /* No space */
+            }
+
+            strcpy(buf, &environ[k][len+1]);
+            pthread_mutex_unlock(&envMtx);
+            return 0;
+        }
+    }
+
+    pthread_mutex_unlock(&envMtx);
+    return ENOENT; /* Doesn't exist */
+}
+
+/******************************************************************************/
 int main(void)
 {
     char * envstr;
@@ -34,4 +89,14 @@ int main(void)
     envstr = getenv("HOME");
     printf("NON-REENTRANT: %s\n", envstr);
 
+    char buf[20];
+
+    if (getenv_r("HOME", buf, 20) == 0) {
+        printf("REENTRANT    : %s\n", envstr);
+    }
+    else {
+        printf("REENTRANT    : FAILED\n");
+    }
+
+    return 0;
 }
