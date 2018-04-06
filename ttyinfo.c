d4a25d64e1b157f9d58d4ca40dc6c3c8300dd89b
Jan Markowski <jan@markowski.ca>
2018-04-06 19:47:33 -0400

    Let's get accurate tty names

--- ttyinfo.c
+++ ttyinfo.c
@@ -2,6 +2,23 @@
 #include <termios.h>
 #include <unistd.h>
 
+char * get_tty_name(int fd)
+{
+    char * name;
+
+    if (isatty(fd)) {
+        name = ttyname(fd);
+
+        if (name == NULL) {
+            name = "undefined";
+        }
+    } else {
+        name = "not a tty";
+    }
+
+    return name;
+}
+
 int main(void)
 {
     char ctermid_name[L_ctermid];
@@ -16,9 +33,10 @@ int main(void)
            "(max len = %d)\n\n", ctermid_name, L_ctermid);
 
     printf("Testing to see which file descriptors refer to a terminal:\n");
-    printf("fd 0: %s\n", isatty(STDIN_FILENO) ? "tty" : "not a tty");
-    printf("fd 1: %s\n", isatty(STDOUT_FILENO) ? "tty" : "not a tty");
-    printf("fd 2: %s\n", isatty(STDERR_FILENO) ? "tty" : "not a tty");
+
+    printf("fd 0: %s\n", get_tty_name(STDIN_FILENO));
+    printf("fd 1: %s\n", get_tty_name(STDOUT_FILENO));
+    printf("fd 2: %s\n", get_tty_name(STDERR_FILENO));
 
     printf("\nUse redirection (e.g. ./ttyinfo.o < /etc/passwd 2> /dev/null)\n"
            "to see how the results may change\n");
